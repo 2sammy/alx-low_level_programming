66861dfb7e7fa36072ae0dbbeca7da44ca55e8c0
2sammy <samuelmanoah61@gmail.com>
2022-03-10 10:32:02 -0800

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <unistd.h>
+
 
 /**
   * main - Prints a text
@@ -7,10 +7,13 @@
   * Return: Always 1 (Success)
   */
 int main(void)
+
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
+
 /**
   * main - Prints a text
   *
@@ -22,6 +25,7 @@ int main(void)
 {
 	
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    
 	return (1);
 
 }
