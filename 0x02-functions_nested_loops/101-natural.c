be10547fa146efc420bd6a04236a6d5a88f9068b
2sammy <samuelmanoah61@gmail.com>
2022-03-15 05:34:06 -0700

    nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,18 +7,18 @@
   */
 int main(void)
 {
-	int i, z = 0;
+int i, z = 0;
 
-	while (i < 1024)
-	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			z += i;
-		}
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
+}
 
-		i++;
-	}
+i++;
+}
 
-	printf("%d\n", z);
-	return (0);
-    
+printf("%d\n", z);
+return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,43 +1,34 @@
+/*
+ * File: 9-times_table.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 
 /**
-  * times_table - Prints the 9 times table, starting with 0
-  *
-  * Return: empty output
-  */
+ * times_table - Prints the 9 times table, starting with 0.
+ */
 void times_table(void)
 {
-	int x, y, z, u, d;
+int num, mult, prod;
 
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
-		{
-			z = x * y;
+for (num = 0; num <= 9; num++)
+{
+_putchar('0');
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
 
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
+prod = num * mult;
 
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-				if (y != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + '0');
 
-				_putchar(z + '0');
-			}
-		}
-
-		_putchar('\n');
-	}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
 }
